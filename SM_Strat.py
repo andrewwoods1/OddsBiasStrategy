07eb8ead3b6193d94c44ecff515ebe18650d2334
andrewwoods1 <38664693+andrewwoods1@users.noreply.github.com>
2018-04-25 17:02:15 +0100

    Create SM_Strat.py

---
+++ SM_Strat.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+path = "C:\\Users\\a0687383\\Downloads\\IRL.csv"
+df = pd.read_csv(path)
+
+
+bank = 1000
+bal = [bank]
+for i in range(len(df)):
+
+    if df['AvgH'][i]**-1 - df['AvgA'][i]**-1 > 0.4:
+        stake = bank * 0.1
+        if df['Res'][i] == 'H':
+            bank = bank + (df['MaxH'][i] - 1)*stake
+        else:
+            bank = bank - stake
+    elif df['AvgH'][i]**-1 - df['AvgA'][i]**-1 < 0.15:
+        stake = bank * 0.1
+        if df['Res'][i] == 'D':
+            bank = bank + (df['MaxD'][i] - 1)*stake
+        else:
+            bank = bank - stake
+    else:
+        bank = bank
+    bal.append(bank)
+
+
+plt.plot(range(len(bal)),bal,'--')
+plt.title("Bank vs Bet No.")
+plt.xlabel("Bet")
+plt.ylabel("Balance")
